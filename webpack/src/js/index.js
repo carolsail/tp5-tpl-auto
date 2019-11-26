22976aca1fb6d5e41acdfab77e3b8f1ce3511f66
sail <1032217152@qq.com>
2019-11-26 17:57:54 +0800

    start

---
+++ webpack/src/js/index.js
@@ -0,0 +1,11 @@
+import '../styles/main.scss'
+import $ from 'jquery'
+import 'bootstrap/scss/bootstrap.scss'
+import table from 'bootstrap-table'
+import 'bootstrap-table/src/bootstrap-table.scss'
+
+console.log('hello from php-webpack-plugin123', table);
+
+$(function(){
+	console.log(123123)
+})
