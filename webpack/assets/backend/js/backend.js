c2ee3817d1305d4c6004715028eca7e2e8abcd65
carolsail <carolsail2013@gmail.com>
2019-12-30 23:31:49 +0800

    css调整

--- webpack/assets/backend/js/backend.js
+++ webpack/assets/backend/js/backend.js
@@ -1,2 +1,13 @@
+import Tabs from '@common/tabs'
+
 const I = require('./' + Config.controller_name)
 I[Config.action_name] != undefined && I[Config.action_name]()
+
+//点击包含.btn-addtabs的元素时新增选项卡
+$(document).on('click', '.btn-addtabs, .addtabsit', function (e) {
+  var that = this;
+  var title = $(that).attr("title") || $(that).data("title") || $(that).data('original-title');
+  var url = $(that).attr('href')
+  Tabs.addtabs(url, title);
+  return false;
+})
--- webpack/assets/backend/js/index.js
+++ webpack/assets/backend/js/index.js
@@ -3,15 +3,6 @@ import Tabs from '@common/tabs'
 import {fixurl, lang as __} from '@common/util';
 
 export function index(){
-    //点击包含.btn-addtabs的元素时新增选项卡
-    $(document).on('click', '.btn-addtabs,.addtabsit', function (e) {
-      var that = this;
-      var title = $(that).attr("title") || $(that).data("title") || $(that).data('original-title');
-      var url = $(that).attr('href')
-      Tabs.addtabs(url, title);
-      return false;
-    })
-
     // 清除缓存
     if($('.wipecache').length){
       $('.wipecache').click(function(){
